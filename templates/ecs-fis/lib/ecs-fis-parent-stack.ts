f192db7778c36b19940f752c2587adb1599124c6
Abhishek Jawali <abhishekjv@gmail.com>
2022-04-20 15:43:23 +0530

    updated

--- templates/ecs-fis/lib/ecs-fis-parent-stack.ts
+++ templates/ecs-fis/lib/ecs-fis-parent-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from "@aws-cdk/core";
-import { ECSStack } from "./ecs-stack"
+import { FisStackEcs } from "./ecs-stack"
 
 export class ECSFIS extends cdk.Stack {
   
@@ -7,7 +7,7 @@ export class ECSFIS extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const ECSStackCDK = new ECSStack(this, "ECSStack");
+    const ECSStackCDK = new FisStackEcs(this, "ECSStack");
 
   }
 };
